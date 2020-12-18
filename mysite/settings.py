b51ebad57c8c5e82a4794f5fa4c91b4c173875e1
Mustafa Ali <mustafa_socool@hotmail.com>
2020-12-18 16:53:46 -0700

    testing

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 django_heroku.settings(locals())
 try:
     from mysite.local_settings import *
